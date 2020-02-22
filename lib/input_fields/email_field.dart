18d1057735400e2b36858b5947aa9f83d3238373
Bohdan Hordiienko <bogdangord125@gamil.com>
2020-02-22 02:04:46 +0200

    add

---
+++ lib/input_fields/email_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class EmailField extends StatefulWidget {
+  @override
+  _EmailFieldState createState() => _EmailFieldState();
+}
+
+class _EmailFieldState extends State<EmailField> {
+  String _email;
+  final _emailKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      key: _emailKey,
+      children: <Widget>[
+        Container(
+          child: TextFormField(
+            style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+            decoration: InputDecoration(
+              icon: Icon(Icons.mail, size: 25, color: Colors.white),
+              labelText: "EMAIL",
+              labelStyle: TextStyle(color: Colors.white),
+              border: InputBorder.none,
+              errorStyle: TextStyle(color: Colors.red[600]),
+              errorBorder: OutlineInputBorder(
+                borderSide: BorderSide(color:Colors.red)
+              )
+            ),
+            // validator: (String value) {
+            //   if (!value.contains("@gmail.com")) {
+            //     return "Ви не ввели E-mail!";
+            //   } else {
+            //     return null;
+            //   }
+            // },
+            onSaved: (String value) {
+              _email = value;
+              print(_email);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import 'package:virtual_app/preview.dart';
+
+
+void main() => runApp(LoginPage());
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstWidget(),
+    );
+  }
+}
+
+class FirstWidget extends StatelessWidget {
+  // final GlobalKey<FormState> _emailKey = GlobalKey<FormState>();
+
+  const FirstWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.indigoAccent[100], Colors.indigoAccent]),
+              ),
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              margin: EdgeInsets.only(top: 100),
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 134,
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Container(
+                        height: 18,
+                        width: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 8,
+                  ),
+                  Container(
+                    width: 134,
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Container(
+                        height: 18,
+                        width: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 140,
+                    margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+                    // color: Colors.black,
+                    child: Text(
+                      "HEBRON",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 140,
+                    child: Text(
+                      "Education Charity Organization",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 100),
+                        child: Text(
+                          "Join Our Family!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40,
+                              letterSpacing: 1),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "Sign up using you'r preferred option.",
+                          style: TextStyle(color: Colors.white54, fontSize: 17),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 100),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: SizedBox(
+                            width: 200,
+                            height: 110,
+                            child: RaisedButton(
+                              color: Colors.indigoAccent,
+                              onPressed: () {
+                                // if (!_emailKey.currentState.validate()) {
+                                //   print("object");
+                                // } else {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => PreviewPage()));
+                                // }
+                              },
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(90),
+                                  bottomRight: Radius.circular(90),
+                                ),
+                              ),
+                              child: Text(
+                                "SIGN IN",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 30),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: SizedBox(
+                            width: 200,
+                            height: 110,
+                            child: RaisedButton(
+                              color: Colors.white,
+                              onPressed: () {
+                                // if (!_emailKey.currentState.validate()) {
+                                //   print("object");
+                                // } else {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => LoginPage()));
+                                // }
+                              },
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(90),
+                                  bottomLeft: Radius.circular(90),
+                                ),
+                              ),
+                              child: Text(
+                                "SIGN UP",
+                                style: TextStyle(
+                                  color: Colors.indigoAccent,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Container(
+                      //   margin:
+                      //       EdgeInsets.only(right: 30, bottom: 30, top: 100),
+                      //   child: Align(
+                      //     alignment: Alignment.bottomRight,
+                      //     child: Text(
+                      //       "Sign Up",
+                      //       style: TextStyle(
+                      //           color: Colors.indigoAccent, fontSize: 20),
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+ 
