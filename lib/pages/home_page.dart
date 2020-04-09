ce5291034eab366e997c48dfaea6cd1ebeddb57e
Bohdan Hordiienko <bogdangord125@gamil.com>
2020-04-09 16:15:04 +0300

    User info page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,17 +18,41 @@ class _HomePageState extends State<HomePage> {
       Center(
         child: Column(
           children: <Widget>[
+            user(context),
             Container(
-              height: screenAwareSize(100, context),
-              width: MediaQuery.of(context).size.width,
+              height: screenAwareSize(350, context),
+              width: screenAwareSize(300, context),
+              decoration: BoxDecoration(
+                color: Colors.white70,
+                border: Border.all(
+                  color: Colors.indigoAccent,
+                  width: screenAwareSize(5, context),
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(
+                      screenAwareSize(10, context),
+                      screenAwareSize(10, context),
+                    ),
+                    blurRadius: screenAwareSize(10, context),
+                  )
+                ],
+              ),
               padding: EdgeInsets.symmetric(
                   horizontal: screenAwareSize(30, context)),
-              color: Colors.indigo[100],
-              child: Text(
-                "Home",
-                style: TextStyle(
-                  color: Colors.indigoAccent,
-                  fontWeight: FontWeight.bold,
+              // color: Colors.indigo[100],
+              child: Center(
+                child: Text(
+                  "Here will be information about you!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
                 ),
               ),
             ),
@@ -57,25 +81,13 @@ class _HomePageState extends State<HomePage> {
       ),
       Center(child: Text("cscsddcs2323dcsc")),
     ];
+
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          //     SliverAppBar(
-          //       leading: Container(),
-          //       title: Text("Title"),
-          //       centerTitle: true,
-          //       backgroundColor: Colors.brown[900],
-          //       expandedHeight: screenAwareSize(190, context),
-          //       floating: true,
-          //       flexibleSpace: FlexibleSpaceBar(
-          //         background: Image.asset(
-          //           "assets/images/image.jpg",
-          //           fit: BoxFit.cover,
-          //         ),
-          //       ),
-          //     ),
           SliverFillRemaining(
-            child: Center(
+            child: GestureDetector(
+              onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -118,5 +130,34 @@ class _HomePageState extends State<HomePage> {
         },
       ),
     );
+  }
+
+  Container user(BuildContext context) {
+    return Container(
+            padding: EdgeInsets.all(screenAwareSize(20, context)),
+            
+            child: ListTile(
+              title: Text(
+                "User Name",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                'example@gmail.com',
+                style: TextStyle(
+                    color: Colors.indigo[200],
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal),
+              ),
+              leading: CircleAvatar(
+                child: Icon(
+                  Icons.perm_identity,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          );
   }
 }
